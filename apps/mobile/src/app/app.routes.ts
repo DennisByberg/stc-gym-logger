ec1a466039a6b5d99b4a0cfad5e536dfef7c6280
dennisbyberg <dennis.byberg@hotmail.com>
2026-04-27 21:14:45 +0200

    chore: add exercises page and remove test boilerplate

--- apps/mobile/src/app/app.routes.ts
+++ apps/mobile/src/app/app.routes.ts
@@ -1,13 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
+  // Shows the landing page.
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: "home",
+    loadComponent: () => import("./home/home.page").then((m) => m.HomePage),
   },
+  // Shows the exercises page.
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: "exercises",
+    loadComponent: () =>
+      import("./exercises/exercises.page").then((m) => m.ExercisesPage),
+  },
+  // Redirects the root URL to the landing page.
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
 ];
---
+++ apps/mobile/src/app/exercises/exercises.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { IonContent } from "@ionic/angular/standalone";
+
+@Component({
+  selector: "app-exercises",
+  templateUrl: "exercises.page.html",
+  styleUrls: ["exercises.page.scss"],
+  imports: [IonContent],
+})
+export class ExercisesPage {}
