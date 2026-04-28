9ee7eba2d3d741b14307bb42562b5542e5d33dfc
dennisbyberg <dennis.byberg@hotmail.com>
2026-04-28 11:37:15 +0200

    feat: add mobile navigation menu

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IonApp, IonIcon, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronBack, chevronForward } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [IonApp, IonIcon, IonRouterOutlet, RouterLink],
 })
 export class AppComponent {
-  constructor() {}
+  readonly isMenuOpen = signal(false);
+
+  constructor() {
+    addIcons({ chevronBack, chevronForward });
+  }
+
+  openMenu(): void {
+    this.isMenuOpen.set(true);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
 }
